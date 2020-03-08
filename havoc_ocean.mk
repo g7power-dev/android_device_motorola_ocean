eb724e72ecab64046f3f424b3f1e2597daa403dd
electimon <electimon@gmail.com>
2020-03-08 03:25:26 -0400

    ocean: update fingerprint

--- havoc_ocean.mk
+++ havoc_ocean.mk
@@ -35,7 +35,7 @@ PRODUCT_MODEL := moto g(7) power
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=ocean \
-        PRIVATE_BUILD_DESC="ocean-user 9 PPO29.114-134 fee207 release-keys"
+        PRIVATE_BUILD_DESC="ocean-user 9 PPO29.114-134-7-1 0ec93 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := motorola/ocean/ocean:9/PPO29.114-134/fee207:user/release-keys
+BUILD_FINGERPRINT := motorola/ocean/ocean:9/PPOS29.114-134-7-1/0ec93:user/release-keys
